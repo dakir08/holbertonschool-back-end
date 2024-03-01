84c1b6f6cca239b05d3d8cb4af581baf8216c3cf
Max Le <gop_max@m1.com.sg>
2024-03-01 15:09:56 +1100

    1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,11 +3,27 @@
 returns information about his/her todo list progress
 """
 
+import sys
 import requests
 
 if __name__ == "__main__":
 
-    x = requests.get('https://jsonplaceholder.typicode.com/todos')
+    try:
+        user_info_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}')
+        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos')
 
-    response = x.json()
-    print(response)
+        todos = todos_response.json()
+        user_info = user_info_response.json()
+        user_name = user_info["name"]
+        total_task = len(todos)
+        completed_tasks = []
+
+        for todo in todos:
+            if todo["completed"] is True:
+                completed_tasks.append(todo["title"])
+        
+        print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{total_task}):")
+        for completed_task in completed_tasks:
+            print("\t"+completed_task)
+    except IndexError:
+        print("Invalid id")
