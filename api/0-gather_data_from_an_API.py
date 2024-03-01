c25369efdb024e1cc6077a1c5e68e63cf9ea69f9
Max Le <gop_max@m1.com.sg>
2024-03-01 15:22:38 +1100

    update

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,14 +3,14 @@
 returns information about his/her todo list progress
 """
 
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
-
+    base_url = "https://jsonplaceholder.typicode.com/users"
     try:
-        user_info_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}')
-        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos')
+        user_info_response = requests.get(f'{base_url}/{sys.argv[1]}')
+        todos_response = requests.get(f'{base_url}/{sys.argv[1]}/todos')
 
         todos = todos_response.json()
         user_info = user_info_response.json()
@@ -21,8 +21,9 @@ if __name__ == "__main__":
         for todo in todos:
             if todo["completed"] is True:
                 completed_tasks.append(todo["title"])
-        
-        print(f"Employee {user_name} is done with tasks({len(completed_tasks)}/{total_task}):")
+
+        print(f"Employee {user_name} is done with tasks"\
+              f"({len(completed_tasks)}/{total_task}):")
         for completed_task in completed_tasks:
             print("\t"+completed_task)
     except IndexError:
